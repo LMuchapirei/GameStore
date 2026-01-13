4bc27604a5adc09f7f4964b5405e94e29f31a2b1
Linval Muchapirei <linval.muchapirei@fbc.co.zw>
2026-01-13 11:42:43 +0200

    create db connection and migrations model definition

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,10 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStore.Api.Data.GameStoreContext>(connString);
 var app = builder.Build();
 app.MapGamesEndpoints();
+app.MigrateDb();
  app.Run();
