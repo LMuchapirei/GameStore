5632a91b0f007249f315a58825583f99138d09dd
Linval Muchapirei <linval.muchapirei@fbc.co.zw>
2026-01-13 12:38:47 +0200

    Implement database seeding for game genres and refactor database connection setup

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -10,5 +11,26 @@ public static  class DataExtensions
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = builder.Configuration.GetConnectionString("GameStore");
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                if(!context.Set<Genre>().Any())
+                {
+                    var action = new Genre { Name = "Action" };
+                    var rpg = new Genre { Name = "RPG" };
+                    var strategy = new Genre { Name = "Strategy" };
+                    var racing = new Genre { Name = "Racing" };
+                    var sports = new Genre { Name = "Sports" };
+                    context.Set<Genre>().AddRange(action, rpg, strategy, racing,sports);
+                    context.SaveChanges();
+                }
+            })
+            );
     }
 }
